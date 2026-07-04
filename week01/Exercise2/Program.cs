0dfe5e9809d0c34d8db418d7fa5f6c7c740b724a
asurtokerr-rgb <asurtokerr@gmail.com>
2026-07-04 17:22:35 +0800

    Week1 Exercise2 Core requirements and stretch challenge

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,69 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your grade percentage?");
+        int gradePercentage = int.Parse(Console.ReadLine());
+        string letterGrade = "";
+
+        if (gradePercentage >= 90)
+        {
+            letterGrade = "A";
+            if (gradePercentage < 93)
+            {
+                letterGrade += "-";
+            }
+
+        }
+        if (gradePercentage >= 80 && gradePercentage < 90)
+        {
+            letterGrade = "B";
+            if (gradePercentage < 83)
+            {
+                letterGrade += "-";
+            }
+            else if (gradePercentage >= 87)
+            {
+                letterGrade += "+";
+            }
+        }
+        if (gradePercentage >= 70 && gradePercentage < 80)
+        {
+            letterGrade = "C";
+            if (gradePercentage < 73)
+            {
+                letterGrade += "-";
+            }
+            else if (gradePercentage >= 77)
+            {
+                letterGrade += "+";
+            }
+        }
+        if (gradePercentage >= 60 && gradePercentage < 70)
+        {
+            letterGrade = "D";
+            if (gradePercentage < 63)
+            {
+                letterGrade += "-";
+            }
+            else if (gradePercentage >= 67)
+            {
+                letterGrade += "+";
+            }
+        }
+        if (gradePercentage < 60)
+        {
+            letterGrade = "F";
+        }
+
+        // comment by kerr. Display result
+        Console.WriteLine($"Your Grade is {letterGrade}.");
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congrats, You Passed!");
+        }
+        else
+        {
+            Console.WriteLine("I'm sorry, you have failed. Try again next time.");
+        }
     }
 }
