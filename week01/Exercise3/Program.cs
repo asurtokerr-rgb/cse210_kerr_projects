754cc9bd2bd0ed934a25ecf0f5c8ce1f0295239f
asurtokerr-rgb <asurtokerr@gmail.com>
2026-07-04 20:08:23 +0800

    C# Programming Exercise 3: Loops Core Requirements and Stretch challenge

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Formats.Asn1;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string answer = "";
+        do
+        {
+            Random randomGenerator = new Random();
+
+            int number = randomGenerator.Next(1, 20);
+            int guessNumber = 0;
+            int numberOfGuess = 0;
+            do
+            {
+                Console.WriteLine("What is your guess number?");
+                guessNumber = int.Parse(Console.ReadLine());
+                numberOfGuess += 1;
+                if (guessNumber < number)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guessNumber > number)
+                {
+                    Console.WriteLine("Lower");
+                }
+            } while (guessNumber != number);
+            Console.WriteLine("You guessed it!");
+            Console.WriteLine($"You got it right in {numberOfGuess} guesses");
+            Console.WriteLine("Do you want to play again?");
+            answer = Console.ReadLine();
+        } while (answer == "yes");
+        Console.WriteLine("Bye!");
+
+
     }
 }
