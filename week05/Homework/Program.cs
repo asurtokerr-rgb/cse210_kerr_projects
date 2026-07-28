f146a00424e2df813bef81b6b959b97879b2c168
asurtokerr-rgb <asurtokerr@gmail.com>
2026-07-28 19:24:10 +0800

    W05 Learning Activity: Inheritance

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment = new Assignment("Kerr", "Homework");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment assignment1 = new MathAssignment("Tom", "Mathematics", "4.1", "3-5");
+        Console.WriteLine(assignment1.GetSummary());
+        Console.WriteLine(assignment1.GetHomeworkList());
+
+        WritingAssignment assignment2 = new WritingAssignment("Carl", "Wrting", "Not a Book");
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetWritinginformation());
     }
 }
