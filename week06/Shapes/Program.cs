3c106685581342213d7fe367296481fbb404c910
asurtokerr-rgb <asurtokerr@gmail.com>
2026-08-03 13:06:32 +0800

    W06 Learning Activity: Polymorphism

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Collections.Generic;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Square s = new Square("blue", 4);
+        // Console.WriteLine(s.GetColor());
+        // Console.WriteLine(s.GetArea());
+
+        Rectangle r = new Rectangle("yellow", 5, 6);
+        // Console.WriteLine(r.GetColor());
+        // Console.WriteLine(r.GetArea());
+
+        Circle c = new Circle("red", 7);
+        // Console.WriteLine(c.GetColor());
+        // Console.WriteLine(c.GetArea());
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(s);
+        shapes.Add(r);
+        shapes.Add(c);
+
+        foreach (Shape b in shapes)
+        {
+            string color = b.GetColor();
+            double area = b.GetArea();
+
+            Console.WriteLine($"The color {color} shape has the area of {area}");
+        }
+
     }
 }
